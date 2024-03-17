edba8380362a066736c2bd0a0647e43240ef3abd
W1nLin4n <grechkaartema@gmail.com>
2024-03-17 20:01:20 +0200

    Chinese Zodiacs utility completed

---
+++ Utils/ChineseZodiac.cs
@@ -0,0 +1,25 @@
+﻿namespace DateAppCSharp.Utils;
+
+public enum ChineseZodiac
+{
+    Rat = 0,
+    Ox, 
+    Tiger, 
+    Rabbit, 
+    Dragon, 
+    Snake, 
+    Horse, 
+    Goat, 
+    Monkey, 
+    Rooster, 
+    Dog,
+    Pig
+}
+
+public static class ChineseZodiacHelper
+{
+    public static ChineseZodiac FromDate(DateTime date)
+    {
+        return (ChineseZodiac)((date.Year - 4) % 12);
+    }
+}
